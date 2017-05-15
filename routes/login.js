4d816bb5c1344986eea7544262c85c5a3cc6da19
TnWn <wen.462@hotmail.com>
2017-05-15 11:53:45 +0800

    Frontend protoyping

---
+++ routes/login.js
@@ -0,0 +1,21 @@
+<<<<<<< HEAD
+var express = require('express');
+var router = express.Router();
+
+/* GET home page. */
+router.get('/', function(req, res, next) {
+  res.render('login');
+});
+
+module.exports = router;
+=======
+var express = require('express');
+var router = express.Router();
+
+/* GET home page. */
+router.get('/', function(req, res, next) {
+  res.render('login');
+});
+
+module.exports = router;
+>>>>>>> origin/master
---
+++ routes/users.js
@@ -0,0 +1,21 @@
+<<<<<<< HEAD
+var express = require('express');
+var router = express.Router();
+
+/* GET users listing. */
+router.get('/', function(req, res, next) {
+  res.send('respond with a resource');
+});
+
+module.exports = router;
+=======
+var express = require('express');
+var router = express.Router();
+
+/* GET users listing. */
+router.get('/', function(req, res, next) {
+  res.send('respond with a resource');
+});
+
+module.exports = router;
+>>>>>>> origin/master
